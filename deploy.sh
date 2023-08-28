474b2f2377140804bf2e6f8ee4126da2abf9aef0
Arsalan Syed <arsalansyed96@outlook.com>
2023-08-28 20:29:38 +0100

    Clone TPM plugins repo if not present

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,13 @@
 cp .bashrc ~/.bashrc
+
+
+# tmux
+if [ ! -d "~/.tmux/plugins/tpm" ]; then
+ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
 cp .tmux.conf ~/.tmux.conf
+echo "Copied over tmux configuration"
+tmux kill-server
+tmux source-file ~/.tmux.conf
+
